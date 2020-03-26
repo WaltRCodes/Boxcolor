b91415cf3646a67d51a3a4f82145938608c1aecb
Walter Rada <WaltRCodes@users.noreply.github.com>
2020-03-26 00:29:06 -0400

    turned App to a class

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react'
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+  render() {
+    return (
+      <div className="App">
+        <h1> Box color change .</h1>
+        <div              className="colorBox"
+        style={{backgroundColor: this.state.color}}
+        onClick={this.onChange}
+        >
+        <p>Click Here</p>
+        </div>
+      </div>
+    )
+  }
+}
